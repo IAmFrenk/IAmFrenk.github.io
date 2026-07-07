6b836dfd94fef17adc7ac316753b450a324e9e4e
IAmFrenk <frenkgoes@hotmail.com>
2026-07-07 22:13:16 +0200

    dates

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,12 +2,16 @@ const projects = [
 	{
 		title: "Frenkmod Drafter",
 		description: "A Civilization V drafter used for multiplayer Frenkmod games. With my own custom civs: Mikeronesia, Liechtenstijn, Iekeland, and Frenkrijk.",
-		href: "projects/civ5-drafter/"
+		href: "projects/civ5-drafter/",
+		date: "Sep 1, 2024",
+		datetime: "2024-09-01"
 	},
 	{
 		title: "Donkey Kong 64 Font Typer",
 		description: "A browser-based PNG font typer that renders text from DK64-style symbol images and exports the result.",
-		href: "projects/dk64-font-typer/"
+		href: "projects/dk64-font-typer/",
+		date: "7 July 2026",
+		datetime: "2026-07-07"
 	},
 ];
 
@@ -22,6 +26,9 @@ function createProjectCard(project) {
 	card.innerHTML = `
 		<h3>${project.title}</h3>
 		<p>${project.description}</p>
+		<div class="project-meta">
+			<span><time datetime="${project.datetime}">${project.date}</time></span>
+		</div>
 	`;
 
 	return card;
